3b169d70f8e3963d4a5a67009877e9a7a4d82365
jigasb <sardorbaxtiyorov2134@gmail.com>
2025-06-10 15:10:12 +0500

    Product service update

--- src/main/java/org/exp/primeapp/service/impl/ProductServiceImpl.java
+++ src/main/java/org/exp/primeapp/service/impl/ProductServiceImpl.java
@@ -10,67 +10,50 @@ import org.exp.primeapp.models.entities.Product;
 import org.exp.primeapp.models.repo.AttachmentRepository;
 import org.exp.primeapp.models.repo.CategoryRepository;
 import org.exp.primeapp.models.repo.ProductRepository;
-import org.exp.primeapp.service.interfaces.AttachmentService;
 import org.exp.primeapp.service.interfaces.ProductService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
+
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
-    private final AttachmentService attachmentService;
     private final AttachmentRepository attachmentRepository;
 
     @Override
     public List<ProductRes> getProducts() {
         return productRepository.findBy_active(true)
                 .stream()
-                .map(product -> {
-                    ProductRes productRes = new ProductRes();
-                    productRes.setName(product.getName());
-                    productRes.setPrice(product.getPrice());
-                    productRes.setDescription(product.getDescription());
-                    productRes.setAmount(product.getAmount());
-                    productRes.setCategoryId(product.getCategory().getId());
-                    return productRes;
-                })
+                .map(this::convertToProductRes)
                 .collect(Collectors.toList());
     }
-
 
     @Override
     public ProductRes getProductById(Long id) {
         Product activeProduct = productRepository.findBy_activeTrueAndId(id);
-        ProductRes productRes = new ProductRes();
-        productRes.setName(activeProduct.getName());
-        productRes.setPrice(activeProduct.getPrice());
-        productRes.setDescription(activeProduct.getDescription());
-        productRes.setAmount(activeProduct.getAmount());
-        productRes.setCategoryId(activeProduct.getCategory().getId());
-        return productRes;
+        if (activeProduct == null) {
+            throw new RuntimeException("Mahsulot topilmadi: ID = " + id);
+        }
+        return convertToProductRes(activeProduct);
     }
 
     @Override
     public List<Product> getAdminProducts() {
         return productRepository.findAll();
     }
+
     @Transactional
     @Override
     public void saveProduct(ProductReq productReq) {
         Category category = categoryRepository.findById(productReq.getCategoryId())
                 .orElseThrow(() -> new RuntimeException("Kategoriya topilmadi: ID = " + productReq.getCategoryId()));
-        Optional<Attachment> optionalAttachment = attachmentRepository.findById(productReq.getAttachmentId());
 
-        if(optionalAttachment.isEmpty()) {
-            return;
-        }
-        Attachment attachment = optionalAttachment.get();
+        Attachment attachment = attachmentRepository.findById(productReq.getAttachmentId())
+                .orElseThrow(() -> new RuntimeException("Attachment topilmadi: ID = " + productReq.getAttachmentId()));
 
         Product product = Product.builder()
                 .name(productReq.getName())
@@ -85,9 +68,19 @@ public class ProductServiceImpl implements ProductService {
         productRepository.save(product);
     }
 
-
     @Override
     public void updateProduct(Long id) {
         productRepository.updateActive(id);
+    }
+
+
+    private ProductRes convertToProductRes(Product product) {
+        return new ProductRes(
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getAmount(),
+                product.getCategory().getId()
+        );
     }
 }
