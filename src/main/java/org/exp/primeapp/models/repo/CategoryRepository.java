700879584c8196502a4ddcee4b50c4de0624c255
Suhrob <suhrob_kalandarov@outlook.com>
2025-06-12 21:05:03 +0500

    Updated: cleaning category repository

--- src/main/java/org/exp/primeapp/models/repo/CategoryRepository.java
+++ src/main/java/org/exp/primeapp/models/repo/CategoryRepository.java
@@ -1,15 +1,12 @@
 package org.exp.primeapp.models.repo;
 
-import org.exp.primeapp.dto.responce.CategoryRes;
 import org.exp.primeapp.models.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    @Query("select c from Category c where c._active = ?1")
     List<Category> findBy_active(boolean active);
 
 }
