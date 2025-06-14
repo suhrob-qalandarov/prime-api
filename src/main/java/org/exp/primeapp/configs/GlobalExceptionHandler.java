5c36bb64ed5b9bc53638d5f24da1a730b3c4948f
Suhrob <suhrob_kalandarov@outlook.com>
2025-06-15 00:15:30 +0500

    Added: GlobalExceptionHandler

---
+++ src/main/java/org/exp/primeapp/configs/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package org.exp.primeapp.configs;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.IOException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public void handleEntityNotFound(EntityNotFoundException e, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public void handleIllegalArgument(IllegalArgumentException e, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.getWriter().write(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public void handleGeneralException(Exception e, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        response.getWriter().write("Internal server error");
+    }
+}
