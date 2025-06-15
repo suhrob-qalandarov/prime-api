525b0bb4a07c6b13bffd645899e232cfa9fbb5b0
Suhrob <suhrob_kalandarov@outlook.com>
2025-06-15 11:59:06 +0500

    Fixed: AttachmentRepository

--- src/main/java/org/exp/primeapp/models/repo/AttachmentRepository.java
+++ src/main/java/org/exp/primeapp/models/repo/AttachmentRepository.java
@@ -3,10 +3,12 @@ package org.exp.primeapp.models.repo;
 import org.exp.primeapp.models.entities.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
 
     Optional<Attachment> findByIdAnd_activeTrue(Long id);
