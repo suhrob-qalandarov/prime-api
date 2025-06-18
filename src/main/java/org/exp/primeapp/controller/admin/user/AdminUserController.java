9f8dc31d7b42e96ae3c2e0419455edf759186661
jigasb <sardorbaxtiyorov2134@gmail.com>
2025-06-18 18:12:47 +0500

    Created: AdminUserController, AdminUsersController

---
+++ src/main/java/org/exp/primeapp/controller/admin/user/AdminUserController.java
@@ -0,0 +1,48 @@
+package org.exp.primeapp.controller.admin.user;
+
+import lombok.RequiredArgsConstructor;
+import org.exp.primeapp.models.dto.request.UserUpdateReq;
+import org.exp.primeapp.models.dto.responce.ApiResponse;
+import org.exp.primeapp.models.dto.responce.UserRes;
+import org.exp.primeapp.models.entities.User;
+import org.exp.primeapp.models.repo.UserRepository;
+import org.exp.primeapp.service.interfaces.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.exp.primeapp.utils.Const.*;
+
+@RestController
+@RequestMapping(API + V1 + ADMIN + USER)
+@RequiredArgsConstructor
+public class AdminUserController {
+    private final UserRepository userRepository;
+    private final UserService userService;
+
+    @GetMapping("{user_id}")
+    public ResponseEntity<User> getUser(@PathVariable long user_id) {
+        User user = userRepository.getById(user_id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/{user_id}")
+    public ResponseEntity<ApiResponse> updateUser(@PathVariable Long user_id, @RequestBody UserUpdateReq userReq) {
+        ApiResponse apiResponse = userService.updateUser(user_id, userReq);
+        return new ResponseEntity<>(apiResponse, HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/activate/{user_id}")
+    public ResponseEntity<ApiResponse> activateUser(@PathVariable Long user_id) {
+        ApiResponse apiResponse = userService.activateUser(user_id);
+        return new ResponseEntity<>(apiResponse, HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/deactivate/{user_id}")
+    public ResponseEntity<ApiResponse> deactivateUser(@PathVariable Long user_id) {
+        ApiResponse apiResponse = userService.deactivateUser(user_id);
+        return new ResponseEntity<>(apiResponse, HttpStatus.ACCEPTED);
+    }
+
+
+}
---
+++ src/main/java/org/exp/primeapp/controller/admin/user/AdminUsersController.java
@@ -0,0 +1,28 @@
+package org.exp.primeapp.controller.admin.user;
+
+import lombok.RequiredArgsConstructor;
+import org.exp.primeapp.models.entities.User;
+import org.exp.primeapp.models.repo.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.exp.primeapp.utils.Const.*;
+
+@RestController
+@RequestMapping(API + V1 + ADMIN + USERS)
+@RequiredArgsConstructor
+public class AdminUsersController {
+    private final UserRepository userRepository;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+}
