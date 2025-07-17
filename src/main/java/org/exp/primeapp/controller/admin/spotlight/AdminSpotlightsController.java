b33adb3f132eb62765d0b3c9557e33b0e522d529
Suhrob <suhrob_kalandarov@outlook.com>
2025-07-17 21:52:56 +0500

    Remove unused `AdminSpotlightsService` and clean up `getSpotlights` method in `AdminSpotlightsController`.

--- src/main/java/org/exp/primeapp/controller/admin/spotlight/AdminSpotlightsController.java
+++ src/main/java/org/exp/primeapp/controller/admin/spotlight/AdminSpotlightsController.java
@@ -3,7 +3,6 @@ package org.exp.primeapp.controller.admin.spotlight;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.exp.primeapp.models.dto.responce.admin.spotlight.SimpleSpotlightRes;
-import org.exp.primeapp.service.interfaces.admin.spotlight.AdminSpotlightsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +17,8 @@ import static org.exp.primeapp.utils.Const.*;
 @RequestMapping(API + V1 + ADMIN + SPOTLIGHTS)
 public class AdminSpotlightsController {
 
-    private final AdminSpotlightsService adminSpotlightsService;
-
     @GetMapping
     public ResponseEntity<List<SimpleSpotlightRes>> getSpotlights() {
-        List<SimpleSpotlightRes> categorySpotlights = adminSpotlightsService.getCategorySpotlights();
-        return new ResponseEntity<>(categorySpotlights, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
-
-
 }
