bf4a5dc2514f7c5186b3d5c58252901400cc0619
Suhrob <suhrob_kalandarov@outlook.com>
2025-07-17 19:45:19 +0500

    Annotate `addSpotlight` method with `@Transactional` in `AdminSpotlightServiceImpl` and remove commented annotation.

--- src/main/java/org/exp/primeapp/service/impl/admin/spotlight/AdminSpotlightServiceImpl.java
+++ src/main/java/org/exp/primeapp/service/impl/admin/spotlight/AdminSpotlightServiceImpl.java
@@ -7,7 +7,9 @@ import org.exp.primeapp.models.entities.Spotlight;
 import org.exp.primeapp.repository.SpotlightRepository;
 import org.exp.primeapp.service.interfaces.admin.spotlight.AdminSpotlightService;
 import org.exp.primeapp.utils.AttachmentUtilService;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +18,8 @@ public class AdminSpotlightServiceImpl implements AdminSpotlightService {
     private final SpotlightRepository spotlightRepository;
     private final AttachmentUtilService attachmentUtilService;
 
+    @Transactional
     @Override
-    //@Transactional
     public void addSpotlight(SpotlightReq spotlightReq) {
         Spotlight spotlight = mapToEntity(spotlightReq);
         spotlightRepository.save(spotlight);
