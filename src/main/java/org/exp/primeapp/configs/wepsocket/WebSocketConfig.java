45de0eabff7b390bd2f4c5ec7f3152c2a1b35d2a
jigasb <sardorbaxtiyorov2134@gmail.com>
2025-07-18 14:47:44 +0500

    Update WebSocketConfig to modify allowed origins for `/ws` endpoint

--- src/main/java/org/exp/primeapp/configs/wepsocket/WebSocketConfig.java
+++ src/main/java/org/exp/primeapp/configs/wepsocket/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://prime77.uz:63342")
+                .setAllowedOrigins("https://prime77.uz")
                 .withSockJS();
     }
 
