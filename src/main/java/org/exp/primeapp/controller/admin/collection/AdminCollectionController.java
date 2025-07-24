e91ac05c81382275e474c503d2b847af8981f9c6
Suhrob <suhrob_kalandarov@outlook.com>
2025-07-24 21:49:36 +0500

    Update: restructure AdminCollectionsController and AdminCollectionController
    - Add CRUD endpoints to `AdminCollectionsController` with `@RequestMapping` for `/v1/admin/collections`.
    - Adjust `AdminCollectionController` by adding a `GET` endpoint for a specific ID and updating `@RequestMapping` to align with `/v1/admin/collection`.

--- src/main/java/org/exp/primeapp/controller/admin/collection/AdminCollectionController.java
+++ src/main/java/org/exp/primeapp/controller/admin/collection/AdminCollectionController.java
@@ -3,23 +3,27 @@ package org.exp.primeapp.controller.admin.collection;
 import lombok.RequiredArgsConstructor;
 import org.exp.primeapp.models.dto.request.CollectionReq;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.exp.primeapp.utils.Const.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(API + V1 + COLLECTION)
+@RequestMapping(API + V1 + ADMIN + COLLECTION)
 public class AdminCollectionController {
+
+    @GetMapping("/{collectionId}")
+    public ResponseEntity<?> get(@PathVariable Long collectionId) {
+        return ResponseEntity.ok().build();
+    }
 
     @PostMapping
     public ResponseEntity<Void> save(@RequestBody CollectionReq collectionReq) {
+        return ResponseEntity.ok().build();
+    }
 
-
-
+    @PutMapping("/{collectionId}")
+    public ResponseEntity<Void> update(@PathVariable Long collectionId, @RequestBody CollectionReq collectionReq) {
         return ResponseEntity.ok().build();
     }
 }
