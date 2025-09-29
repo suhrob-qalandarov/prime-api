8a46d928d81f4eb171c67677a5af52fbf6655338
Suhrob <suhrob_kalandarov@outlook.com>
2025-09-29 23:42:07 +0500

    Simplify `UserController` by removing redundant endpoints and consolidating `getUserById`.

--- src/main/java/org/exp/primeapp/controller/user/UserController.java
+++ src/main/java/org/exp/primeapp/controller/user/UserController.java
@@ -2,10 +2,8 @@ package org.exp.primeapp.controller.user;
 
 import lombok.RequiredArgsConstructor;
 import org.exp.primeapp.models.dto.responce.user.UserRes;
-import org.exp.primeapp.models.entities.User;
 import org.exp.primeapp.service.interfaces.user.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import static org.exp.primeapp.utils.Const.*;
@@ -17,34 +15,9 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/me")
-    public ResponseEntity<UserRes> getUserData(@AuthenticationPrincipal User user) {
-        UserRes userRes = userService.getUserData(user);
-        return ResponseEntity.ok(userRes);
-    }
-
-    @GetMapping("/by/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<UserRes> getUserById(@PathVariable Long id) {
         UserRes user = userService.getById(id);
-        return ResponseEntity.ok(user);
-    }
-
-    @GetMapping("/by/{telegramId}")
-    public ResponseEntity<UserRes> getUserByTelegramId(@PathVariable Long telegramId) {
-        UserRes user = userService.getByTelegramId(telegramId);
-        return ResponseEntity.ok(user);
-    }
-
-    @GetMapping("/by/{phoneNumber}")
-    public ResponseEntity<UserRes> getUserByPhoneNumber(@PathVariable String phoneNumber) {
-        UserRes user = userService.getByPhoneNumber(phoneNumber);
-        return ResponseEntity.ok(user);
-    }
-
-    // it's conflicted end-point
-    @GetMapping("/by/{username}")
-    public ResponseEntity<UserRes> getUserByUserName(@PathVariable String username) {
-        UserRes user = userService.getByUsername(username);
         return ResponseEntity.ok(user);
     }
 }
