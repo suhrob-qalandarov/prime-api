bf43997e4497bd6ebc2fb96e273666c7a373e7fe
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-21 10:49:17 +0500

    Add `findAllByKeyIn` method to `AttachmentRepository`.

--- src/main/java/org/exp/primeapp/repository/AttachmentRepository.java
+++ src/main/java/org/exp/primeapp/repository/AttachmentRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,4 +46,6 @@ public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
     int countAllByActiveFalse();
 
     Attachment findByKey(String key);
+
+    List<Attachment> findAllByKeyIn(Collection<String> keys);
 }
