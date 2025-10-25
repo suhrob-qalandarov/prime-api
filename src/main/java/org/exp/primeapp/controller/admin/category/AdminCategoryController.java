317a917b34bce92c8f39295405e8de6c07cf1d9a
Suhrob <suhrob_kalandarov@outlook.com>
2025-10-25 22:07:33 +0500

    Added pre auth to category controller and path version changed to v2

--- src/main/java/org/exp/primeapp/controller/admin/category/AdminCategoryController.java
+++ src/main/java/org/exp/primeapp/controller/admin/category/AdminCategoryController.java
@@ -7,6 +7,7 @@ import org.exp.primeapp.models.dto.responce.admin.AdminCategoryRes;
 import org.exp.primeapp.service.face.user.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,25 +16,28 @@ import java.util.Map;
 import static org.exp.primeapp.utils.Const.*;
 
 @RestController
-@RequestMapping(API + V1 + ADMIN + CATEGORY)
+@RequestMapping(API + V2 + ADMIN + CATEGORY)
 @RequiredArgsConstructor
 public class AdminCategoryController {
 
     private final CategoryService categoryService;
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<AdminCategoryRes> createCategory(@RequestBody CategoryReq categoryReq) {
         AdminCategoryRes adminCategoryRes = categoryService.saveCategory(categoryReq);
         return new ResponseEntity<>(adminCategoryRes, HttpStatus.CREATED);
     }
 
     @GetMapping("/{categoryId}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'VISITOR')")
     public ResponseEntity<AdminCategoryRes> getCategoryById(@PathVariable Long categoryId) {
         AdminCategoryRes adminCategoryRes  = categoryService.getAdminCategoryResById(categoryId);
         return new ResponseEntity<>(adminCategoryRes, HttpStatus.OK);
     }
 
     @PutMapping("/{categoryId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<AdminCategoryRes> updateCategory(
             @PathVariable Long categoryId,
             @RequestBody CategoryReq categoryReq
@@ -43,24 +47,28 @@ public class AdminCategoryController {
     }
 
     @GetMapping("/dashboard")
+    @PreAuthorize("hasAnyRole('ADMIN', 'VISITOR')")
     public ResponseEntity<AdminCategoryDashboardRes> getCategoryDashboard() {
         AdminCategoryDashboardRes categoryDashboardRes = categoryService.getCategoryDashboardRes();
         return new ResponseEntity<>(categoryDashboardRes, HttpStatus.ACCEPTED);
     }
 
     @PatchMapping("/order")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<AdminCategoryRes>> updateCategoriesOrder(@RequestBody Map<Long, Long> categoryOrderMap) {
         List<AdminCategoryRes> updatedCategories = categoryService.updateCategoryOrder(categoryOrderMap);
         return new ResponseEntity<>(updatedCategories, HttpStatus.ACCEPTED);
     }
 
     @PatchMapping("/toggle/{categoryId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> toggleCategory(@PathVariable Long categoryId) {
         categoryService.toggleCategoryActiveStatus(categoryId);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
     @PatchMapping("/toggle-with-products/{categoryId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Void> toggleCategoryWithProducts(@PathVariable Long categoryId) {
         categoryService.toggleCategoryActiveStatusWithProductActiveStatus(categoryId);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
